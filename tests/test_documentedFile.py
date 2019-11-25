576ab795edf27e0bb46a709568dde408c799b22b
Viktor Ovsiychuk <victor.ovseychuk@gmail.com>
2019-11-25 18:42:06 +0200

    fixed unit tests

--- tests/test_documentedFile.py
+++ tests/test_documentedFile.py
@@ -10,14 +10,22 @@ class TestDocumentedFile(TestCase):
         file.package = 'org.test.package.core.x'
         file.imports = ['org.test.package.Kek', 'org.test.package.Class']
 
-        self.assertEqual(file.get_doc_import_path('Class'), '../../Class.java.html')
+        file_list = [DocumentedFile()]
+        file_list[0].package = 'org.test.package'
+        file_list[0].file_path = 'org/test/package/Class.java'
+
+        self.assertEqual(file.get_doc_import_path('Class', file_list), '../../Class.java.html')
 
     def test_get_import_path_2(self):
         file = DocumentedFile()
         file.package = 'org.test.package'
         file.imports = ['org.test.package.Kek', 'org.test.package.core.Class']
 
-        self.assertEqual(file.get_doc_import_path('Class'), 'core/Class.java.html')
+        file_list = [DocumentedFile()]
+        file_list[0].package = 'org.test.package.core'
+        file_list[0].file_path = 'org/test/package/core/Class.java'
+
+        self.assertEqual(file.get_doc_import_path('Class', []), 'core/Class.java.html')
 
     def test_get_import_path_disjoint(self):
         file = DocumentedFile()
@@ -25,4 +33,4 @@ class TestDocumentedFile(TestCase):
         file.imports = ['java.util.JavaClass', 'org.test.Class']
 
         # or return link to online java doc
-        self.assertEqual(file.get_doc_import_path('JavaClass'), None)
+        self.assertEqual(file.get_doc_import_path('JavaClass', []), None)
